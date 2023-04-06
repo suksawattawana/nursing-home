bee4d2a5e28811fc89e42f8d47f321ddcfaa42ee
SuksawatTawana <108627429+suksawattawana@users.noreply.github.com>
2023-04-06 23:39:28 +0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './component/content/home/home.component';
+import { CenterprincipleComponent } from './component/content/centerprinciple/centerprinciple.component';
+import { ServicerateComponent } from './component/content/servicerate/servicerate.component';
+import { SymptomreportComponent } from './component/content/symptomreport/symptomreport.component';
+
+
+const routes: Routes = [
+  { path: 'rout-home', component: HomeComponent },
+  { path: 'rout-centerprinciple', component: CenterprincipleComponent },
+  { path: 'rout-servicerate', component: ServicerateComponent },
+  { path: 'rout-symptomreport', component: SymptomreportComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { SymptomreportComponent } from './component/content/symptomreport/sympto
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
